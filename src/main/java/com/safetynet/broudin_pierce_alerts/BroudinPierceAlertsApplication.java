6bbf0c499ad4903fdc2f7752042c59e69077facc
Pierce Broudin <piercebroudin.pb@gmail.com>
2025-03-05 16:05:28 +0100

    Initial commit on dev branch

---
+++ src/main/java/com/safetynet/broudin_pierce_alerts/BroudinPierceAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.broudin_pierce_alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroudinPierceAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroudinPierceAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/broudin_pierce_alerts/BroudinPierceAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.broudin_pierce_alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroudinPierceAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
